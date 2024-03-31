9936de7af8d712c667501fa3517b0f5e0eb3f07b
bikashdas0108 <bikashdas0108@gmail.com>
2024-03-31 12:04:06 +0530

    project and about section

---
+++ src/projects/projects.js
@@ -0,0 +1,83 @@
+const projectsContainerElement = document.getElementById("projectsContainer");
+
+const getColor = (key) => {
+  switch (key) {
+    case "ReactJs":
+      return "color: lightGreen";
+    case "Redux":
+      return "color: yellow";
+    case "TypeScript":
+      return "color: orangered";
+    default:
+      return "color: lightBlue";
+  }
+};
+
+const projects = [
+  {
+    title: "YouTube Clone",
+    image: "src/images/yt-clone.jpeg",
+    description: [
+      "Developed a web application for online food ordering by using React.js, Node.js, MongoDB and Express.js.",
+      "Implemented features such as a robust search functionality for food items and categories, allowing users to easily find desired items.",
+      "Developed an intuitive cart system that enables users to add items with their desired quantities for seamless ordering.",
+      "Implemented a comprehensive order history feature to track and maintain records of all orders placed to date.",
+    ],
+    tech: ["ReactJs", "Redux", "TypeScript"],
+  },
+  {
+    title: "My Game",
+    image: "src/images/yt-clone.jpeg",
+    description: [
+      "Developed a web application for online food ordering by using React.js, Node.js, MongoDB and Express.js.",
+      "Implemented features such as a robust search functionality for food items and categories, allowing users to easily find desired items.",
+      "Developed an intuitive cart system that enables users to add items with their desired quantities for seamless ordering.",
+      "Implemented a comprehensive order history feature to track and maintain records of all orders placed to date.",
+    ],
+    tech: ["ReactJs", "Redux", "TypeScript"],
+  },
+  {
+    title: "Swamato",
+    image: "src/images/yt-clone.jpeg",
+    description: [
+      "Developed a web application for online food ordering by using React.js, Node.js, MongoDB and Express.js.",
+      "Implemented features such as a robust search functionality for food items and categories, allowing users to easily find desired items.",
+      "Developed an intuitive cart system that enables users to add items with their desired quantities for seamless ordering.",
+      "Implemented a comprehensive order history feature to track and maintain records of all orders placed to date.",
+    ],
+    tech: ["ReactJs", "Redux", "TypeScript"],
+  },
+];
+
+const showProjects = () => {
+  const projectsHtml = projects.map((project) => {
+    const descriptionList = project.description?.map(
+      (listItem) => `<li>${listItem}</li>`
+    );
+
+    const techList = project.tech?.map(
+      (techItem) =>
+        `<span class='tech' style='${getColor(techItem)}'>#${techItem}</span>`
+    );
+
+    return `<div class='project'>
+            <div id="${project.title}-image"></div>
+            <div class='project-title'>${project.title}</div>
+            <div class='tech-list'>${techList?.join("")}</div>
+            <ul>${descriptionList?.join("")}</ul>
+        </div>`;
+  });
+
+  projectsContainerElement.innerHTML = projectsHtml.join("");
+
+  projects.forEach((project) => {
+    let image = document.createElement("img");
+    image.src = project.image;
+    image.alt = `${project.title}-image`;
+    image.className = "project-image";
+    const logoElement = document.getElementById(`${project.title}-image`);
+    logoElement.appendChild(image);
+  });
+};
+
+showProjects();
